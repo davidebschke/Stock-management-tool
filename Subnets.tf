acf77ac59480bbabec7e228837fa221245a2c34d
David Ebschke <80986213+davidebschke@users.noreply.github.com>
2022-11-22 15:25:10 +0100

    add lager und Planung subnet

--- Subnets.tf
+++ Subnets.tf
@@ -40,3 +40,53 @@ resource "aws_subnet" "Personalabteilung-Ersatz" {
     Name = "Personalabteilung-Ersatz"
   }
 }
+
+resource "aws_subnet" "Planung" {
+  vpc_id     = aws_vpc.opt-vpc.vpc_id
+  cidr_block = "10.64.0.0/8"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "Planung"
+  }
+}
+
+resource "aws_subnet" "Planung-Ersatz" {
+  vpc_id     = aws_vpc.opt-vpc.vpc_id
+  cidr_block = "10.80.0.0/8"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "Planung-Ersatz"
+  }
+}
+
+resource "aws_subnet" "Personalabteilung-Ersatz" {
+  vpc_id     = aws_vpc.opt-vpc.vpc_id
+  cidr_block = "10.48.0.0/8"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "Personalabteilung-Ersatz"
+  }
+}
+
+resource "aws_subnet" "Lager" {
+  vpc_id     = aws_vpc.opt-vpc.vpc_id
+  cidr_block = "10.96.0.0/8"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "Lager"
+  }
+}
+
+resource "aws_subnet" "Lager-Ersatz" {
+  vpc_id     = aws_vpc.opt-vpc.vpc_id
+  cidr_block = "10.112.0.0/8"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "Lager-Ersatz"
+  }
+}
