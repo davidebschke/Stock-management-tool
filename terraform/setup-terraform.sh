70dc3fc375197d5e285e8739b6d96ec3592c702e
David Ebschke <80986213+davidebschke@users.noreply.github.com>
2022-11-23 15:07:54 +0100

    Empty commit

--- terraform/setup-terraform.sh
+++ terraform/setup-terraform.sh
@@ -1,23 +1,9 @@
 S3_BUCKET_NAME=terraform-state-12312d123fa
 REGION=us-west-2
-S3_BUCKET_WAREHOUSE_NAME=warehousebucket
-S3_BUCKET_TECHNICIAN_DEPLOYMENT_NAME= technician_deploymentbucket
 
 if aws s3 ls "s3://$S3_BUCKET_NAME" 2>&1 | grep -q 'An error occurred'
 then
     aws s3api create-bucket --bucket $S3_BUCKET_NAME --region $REGION --create-bucket-configuration LocationConstraint=$REGION
 else
-    echo "log bucket already exists"
-
-if aws s3 ls "s3://$S3_BUCKET_WAREHOUSE_NAME" 2>&1 | grep -q 'An error occurred'
-then
-    aws s3api create-bucket --bucket $S3_BUCKET_WAREHOUSE_NAME --region $REGION --create-bucket-configuration LocationConstraint=$REGION
-else
-    echo "Warehouse bucket already exists"
-
-if aws s3 ls "s3://$S3_BUCKET_TECHNICIAN_DEPLOYMENT_NAME" 2>&1 | grep -q 'An error occurred'
-then
-    aws s3api create-bucket --bucket $S3_BUCKET_TECHNICIAN_DEPLOYMENT_NAME --region $REGION --create-bucket-configuration LocationConstraint=$REGION
-else
-    echo "Technician Deployment bucket already exists"
+    echo "bucket already exists"
 fi
