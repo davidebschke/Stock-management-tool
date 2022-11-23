bfec37a15079fe087c26290c720ce96a0889a8d4
David Ebschke <80986213+davidebschke@users.noreply.github.com>
2022-11-23 15:32:37 +0100

    right names for the each key of subnets

--- terraform/Subnets.tf
+++ terraform/Subnets.tf
@@ -19,16 +19,15 @@ resource "aws_subnet" "name" {
 for_each = {
 "management_a" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[0] ,availabilityZone="us-west-2a"}
 "management_b" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[1] ,availabilityZone="us-west-2b"}
-"management_c" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[2] ,availabilityZone="us-west-2a"}
-"management_d" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[3] ,availabilityZone="us-west-2b"}
-"management_e" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[4] ,availabilityZone="us-west-2a"}
-"management_f" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[5] ,availabilityZone="us-west-2b"}
-"management_g" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[6] ,availabilityZone="us-west-2a"}
-"management_h" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[7] ,availabilityZone="us-west-2b"}
-"management_i" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[8] ,availabilityZone="us-west-2a"}
-"management_j" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[9] ,availabilityZone="us-west-2b"}
+"hr_department_a" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[2] ,availabilityZone="us-west-2a"}
+"hr_department_b" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[3] ,availabilityZone="us-west-2b"}
+"organization_a" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[4] ,availabilityZone="us-west-2a"}
+"organization_b" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[5] ,availabilityZone="us-west-2b"}
+"warehouse_a" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[6] ,availabilityZone="us-west-2a"}
+"warehouse_b" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[7] ,availabilityZone="us-west-2b"}
+"technician_deployment_a" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[8] ,availabilityZone="us-west-2a"}
+"technician_deployment_b" = {vpc_id= aws_vpc.opt-vpc.id ,cidr-Block= var.cidrBlock[9] ,availabilityZone="us-west-2b"}
 }
-
   vpc_id = each.value.vpc_id
   cidr_block = each.value.cidr-Block
   availability_zone = each.value.availabilityZone
