20f385705301cdf704b05562d8514cdc131af52d
David Ebschke <80986213+davidebschke@users.noreply.github.com>
2022-11-25 10:03:16 +0100

    outsource the Subnet variable for cidr block. A wish from Irene

--- terraform/dynamodbwarehouse.tf
+++ terraform/dynamodbwarehouse.tf
@@ -1,22 +1,28 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "WarehouseDB"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
+  read_capacity  = 20
+  write_capacity = 20
   hash_key       = "objectID"
+  range_key = "Goods Name"
 
-  attribute {
-    name = "objectID"
-    type = "S"
-  }
+    attribute {
+        name = "objectID"
+        type = "S"
+    }
 
-  attribute {
-    name = "Goods Name"
-    type = "S"
-  }
+    attribute {
+        name = "Goods Name"
+        type = "S"
+    }
 
-  attribute {
-    name = "Piece of goods"
-    type = "N"
-  }
+    attribute {
+        name = "Piece of goods"
+        type = "N"
+    }
+
+    attribute {
+        name="Price of goods"
+        type="N"
+    }
 }
