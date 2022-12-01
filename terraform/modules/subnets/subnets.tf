2384b1b754eba3c79fe2bbd500159fd7f3176ed3
David Ebschke <80986213+davidebschke@users.noreply.github.com>
2022-12-01 05:45:28 +0100

    formatting all files with shortcut

--- terraform/modules/subnets/subnets.tf
+++ terraform/modules/subnets/subnets.tf
@@ -1,10 +1,10 @@
 resource "aws_subnet" "subnets_hr" {
-for_each =  {for subnet in var.subnets:subnet.name => subnet }
+  for_each = { for subnet in var.subnets : subnet.name => subnet }
 
-    vpc_id = var.vpc_id
-    cidr_block = each.value.cidr_block
-    availability_zone = each.value.availability_zone
-    tags={
-        Name = each.value.name
-    }
+  vpc_id            = var.vpc_id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+  tags = {
+    Name = each.value.name
+  }
 }
