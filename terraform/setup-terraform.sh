0b3729b17d7efc2c70693b947b2de58e9fb869ac
David Ebschke <80986213+davidebschke@users.noreply.github.com>
2022-12-01 09:22:12 +0100

    test

--- terraform/setup-terraform.sh
+++ terraform/setup-terraform.sh
@@ -2,7 +2,7 @@
 
 set -a BucketNames capstone-terraform-state warehousebucketawscapstone techniciandeploymentbucket;
 REGION=us-west-2
-arraylength=${#BucketNames[@]}
+arraylength=3
 
 for ((i=0; i<${arraylength};i++)); 
 do 
