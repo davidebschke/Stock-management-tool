c1861b6e21353ad713d3f06becec425abadd257a
David Ebschke <80986213+davidebschke@users.noreply.github.com>
2022-12-01 09:11:51 +0100

    delete declare

--- terraform/setup-terraform.sh
+++ terraform/setup-terraform.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a BucketNames= capstone-terraform-state warehousebucketawscapstone techniciandeploymentbucket;
+BucketNames= capstone-terraform-state warehousebucketawscapstone techniciandeploymentbucket;
 REGION=us-west-2
 arraylength=${#BucketNames[@]}
 
