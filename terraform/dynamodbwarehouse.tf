2ef8a5c34bb6edb798d2b2399fd18dc3d99357d3
David Ebschke <80986213+davidebschke@users.noreply.github.com>
2022-12-06 12:14:31 +0100

    Change the names and keys DB  to lowercase

--- terraform/dynamodbwarehouse.tf
+++ terraform/dynamodbwarehouse.tf
@@ -1,5 +1,5 @@
-resource "aws_dynamodb_table" "WarehouseDB" {
-  name           = "WarehouseDB"
+resource "aws_dynamodb_table" "warehouseDB" {
+  name           = "warehousedb"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
@@ -11,29 +11,29 @@ resource "aws_dynamodb_table" "WarehouseDB" {
   }
 
   attribute {
-    name = "Goods_Name"
+    name = "goods_name"
     type = "S"
   }
 
 global_secondary_index {
-    name = "IndexForGoodsName"
-    hash_key = "Goods_Name"
+    name = "index_for_goods_name"
+    hash_key = "goods_name"
     write_capacity = 10
     read_capacity = 10
     projection_type = "INCLUDE"
-    non_key_attributes = ["Amount_of_goods", "price_of_goods"]
+    non_key_attributes = ["amount_of_goods", "price_of_goods"]
     }
 }
 
 # Example Item to check the Table
 resource "aws_dynamodb_table_item" "warehouse_item_example" {
-    table_name = aws_dynamodb_table.WarehouseDB.name
-    hash_key   = aws_dynamodb_table.WarehouseDB.hash_key
+    table_name = aws_dynamodb_table.warehouseDB.name
+    hash_key   = aws_dynamodb_table.warehouseDB.hash_key
   item = <<ITEM
   {
     "objectID": {"S": "1"},
-    "Goods_Name": {"S": "Nagel"},
-    "Amount_of_goods": {"S": "200"},
+    "goods_name": {"S": "Nagel"},
+    "amount_of_goods": {"S": "200"},
     "price_of_goods":{"S": "2,5"}
   }
 ITEM
