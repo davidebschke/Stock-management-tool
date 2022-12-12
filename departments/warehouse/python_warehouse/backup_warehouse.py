d69f8cf6352d2945225d6943f12d7c30e106df82
David Ebschke <80986213+davidebschke@users.noreply.github.com>
2022-12-12 13:05:24 +0100

    lambda function a bit better

--- departments/warehouse/python_warehouse/backup_warehouse.py
+++ departments/warehouse/python_warehouse/backup_warehouse.py
@@ -1,10 +1,58 @@
+import json
+import traceback
+import boto3
+
+def get_data(table_name, client):
+    """
+    Get data from DyanamoDB
+    """
+    results = []
+    last_evaluated_key = None
+    while True:
+        if last_evaluated_key:
+            response = client.scan(
+                TableName=table_name,
+                ExclusiveStartKey=last_evaluated_key
+            )
+        else: 
+            response = client.scan(TableName=table_name)
+        last_evaluated_key = response.get('LastEvaluatedKey')
+        
+        results.extend(response['Items'])
+        
+        if not last_evaluated_key:
+            break
+    return results
+
 def lambda_handler(event, context):
 
-   message = 'Hello {} !'.format(event['key1'])
+    statusCode = 200
+    statusMessage = 'Success'
 
-   return {
+    try:
+        # parse the payload
+        tableName = event['warehousedb']
+        s3_bucket = event['warehousebucketawscapstone']
+        s3_object = event['database']
+        filename = event['example.txt']
 
-       'message' : message
+        # scan the dynamodb
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(tableName)
+        
+        client = boto3.client('dynamodb')
+        data = get_data(tableName, client)
+            
+        # export JSON to s3 bucket
+        s3 = boto3.resource('s3')
+        s3.Object(s3_bucket, s3_object + filename).put(Body=json.dumps(data))
 
-   }
+    except Exception as e:
+            statusCode = 400
+            statusMessage = traceback.format_exc()
+
+    return {
+        "statusCode": statusCode,
+        "status": statusMessage
+    }
    
