567c2cbd8f8cf579c2cdb159b250e3eb3d88b74e
David Ebschke <80986213+davidebschke@users.noreply.github.com>
2022-12-15 12:14:39 +0100

    change names

--- terraform/departments/warehouse/lambda_function.tf
+++ terraform/departments/warehouse/lambda_function.tf
@@ -1,11 +1,11 @@
 
-data "archive_file" "zip_the_python_code" {
+data "archive_file" "zip_the_export_lambda" {
     type        = "zip"
     source_dir  = "./departments/warehouse/python_warehouse/"
     output_path = "./departments/warehouse/python_warehouse_build/python_warehouse.zip"
 }
 
-resource "aws_lambda_function" "terraform_lambda_func" {
+resource "aws_lambda_function" "terraform_lambda_func_export" {
     filename                       = "./departments/warehouse/python_warehouse_build/python_warehouse.zip"
     function_name                  = "Warehouse_Lambda_function"
     role                           = "arn:aws:iam::886389208156:role/LabRole"
--- terraform/departments/warehouse/lambda_trigger.tf
+++ terraform/departments/warehouse/lambda_trigger.tf
@@ -7,13 +7,13 @@ resource "aws_cloudwatch_event_rule" "every_five_minutes" {
 resource "aws_cloudwatch_event_target" "check_foo_every_five_minutes" {
     rule = aws_cloudwatch_event_rule.every_five_minutes.name
     target_id = "export_csv"
-    arn = aws_lambda_function.terraform_lambda_func.arn
+    arn = aws_lambda_function.terraform_lambda_func_export.arn
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
     statement_id = "AllowExecutionFromCloudWatch"
     action = "lambda:InvokeFunction"
-    function_name = aws_lambda_function.terraform_lambda_func.function_name
+    function_name = aws_lambda_function.terraform_lambda_func_export.function_name
     principal = "events.amazonaws.com"
     source_arn = aws_cloudwatch_event_rule.every_five_minutes.arn
 }
