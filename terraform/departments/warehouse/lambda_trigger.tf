0b928510883bab3fae004d74620e2c3b43ffde3c
David Ebschke <80986213+davidebschke@users.noreply.github.com>
2022-12-15 11:46:53 +0100

    add lambda trigger

---
+++ terraform/departments/warehouse/lambda_trigger.tf
@@ -0,0 +1,19 @@
+resource "aws_cloudwatch_event_rule" "every_five_minutes" {
+    name = "every-five-minutes"
+    description = "Fires every five minutes"
+    schedule_expression = "rate(5 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "check_foo_every_five_minutes" {
+    rule = aws_cloudwatch_event_rule.every_five_minutes.name
+    target_id = "export csv"
+    arn = aws_lambda_function.terraform_lambda_func
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.terraform_lambda_func.function_name
+    principal = "events.amazonaws.com"
+    source_arn = aws_cloudwatch_event_rule.every_five_minutes.arn
+}
